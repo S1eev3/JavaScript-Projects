b79e09dae595c0bf5d5db0e16193468a860a39d8
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 18:54:36 -0500

    isNaN functions

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/javascript/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/javascript/main.js
@@ -1,2 +1,14 @@
 document.write(typeof "Word");
 document.write("10" + 5);
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0 / 0;
+}
+
+function true_function() {
+    document.getElementById("true").innerHTML = isNaN('This is a string');
+}
+
+function false_function() {
+    document.getElementById("false").innerHTML = isNaN(007);
+}

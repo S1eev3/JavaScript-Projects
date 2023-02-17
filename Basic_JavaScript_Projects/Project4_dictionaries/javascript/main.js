69b80c909750d8bf95273ecb8a38b132ace7ba8f
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 18:33:43 -0500

    js comments

--- Basic_JavaScript_Projects/Project4_dictionaries/javascript/main.js
+++ Basic_JavaScript_Projects/Project4_dictionaries/javascript/main.js
@@ -1,11 +1,12 @@
-function myDictionary() {
-    var Animal = {
+function myDictionary() {   //named function myDictionary
+    var Animal = {          //Started dictionary for variable Animal
         Species: "Dog",
         Color: "Yellow",
         Breed: "Labrador",
         Name: "Cooper",
         Age: 4,
-        Sound: "Bark!"
+        Sound: "Bark!"      //Last KVP for Animal Dictionary
     };
-    document.getElementById("Dictionary").innerHTML = Animal.Name;
+    delete Animal.Sound;    //Delete sound KVP
+    document.getElementById("Dictionary").innerHTML = Animal.Sound; //Attempt to use deleted KVP in HTML event
 }

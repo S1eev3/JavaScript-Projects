9c7e7cd52d6e4c5085522570c8f96839669cd192
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 17:37:49 -0500

    multiplication and division functions

--- Basic JavaScript Projects/Project3_math_operators/javascript/math_script.js
+++ Basic JavaScript Projects/Project3_math_operators/javascript/math_script.js
@@ -7,3 +7,13 @@ function subtraction_Function() {
     var subtraction = 5 - 2;
     document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
 }
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Operators").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Operators").innerHTML = "48 divided by 6 = " + simple_Math;
+}

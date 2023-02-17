24890396ec16828387998c75b37c0d7bbd0ff9dc
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 14:29:16 -0500

    Basic javascript project submission

--- Basic JavaScript Projects/javascript/main.js
+++ Basic JavaScript Projects/javascript/main.js
@@ -1,2 +1,9 @@
-var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermione",
-    Daughter = "Penny", Son = "Zorro";
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermione", //Defining variables for family Arezzini
+    Daughter = "Penny", Son = "Zorro";                            //Continued defining variables
+
+var Sent1 = "This is the beginning of the string"   //Define variable Sent1
+var Sent2 = " and this is the end of the string."   //Define variable Sent2 
+
+window.alert(Family);   //Alert box printing family name
+
+document.write(Sent1 + Sent2);  //Concatenating variable Sent1 and Sent2 and printing to screen

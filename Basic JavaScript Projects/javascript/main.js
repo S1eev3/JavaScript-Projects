4f59fb7c5e3c61fdbc82772be2c0e998761f757c
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-16 21:35:36 -0500

    main.js challenges

---
+++ Basic JavaScript Projects/javascript/main.js
@@ -0,0 +1,6 @@
+var X = "This is a string of test text";
+window.alert(X);
+document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write("\"Be whoe you are and say what you feel,"
+    + " because those you mind don\'t matter and those who matter don\'t mind.\""
+    + "-Dr.Suess");

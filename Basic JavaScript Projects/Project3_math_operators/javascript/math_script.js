ec69a73f66c2c97c9767c7712044ec8702dd09e4
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 17:32:17 -0500

    subtraction_Function

--- Basic JavaScript Projects/Project3_math_operators/javascript/math_script.js
+++ Basic JavaScript Projects/Project3_math_operators/javascript/math_script.js
@@ -2,3 +2,8 @@ function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML ="2 + 2 = " + addition;
 }
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+}

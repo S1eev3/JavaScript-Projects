b7c9d34c5920559f8b27d67d37801648e25b088f
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-16 22:04:11 -0500

    challenges

--- Basic JavaScript Projects/javascript/main.js
+++ Basic JavaScript Projects/javascript/main.js
@@ -1,6 +1,2 @@
-var X = "This is a string of test text";
-window.alert(X);
-document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
-document.write("\"Be whoe you are and say what you feel,"
-    + " because those you mind don\'t matter and those who matter don\'t mind.\""
-    + "-Dr.Suess");
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermione",
+    Daughter = "Penny", Son = "Zorro";

357768c97bfd741d059f374bb3684a6f279384a1
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-17 20:08:07 -0500

    age verification function

--- Basic_JavaScript_Projects/Project6_ternary_operators_constructors/javascript/main.js
+++ Basic_JavaScript_Projects/Project6_ternary_operators_constructors/javascript/main.js
@@ -4,3 +4,10 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function Age_Verification() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are too young" : "You are old enough";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}

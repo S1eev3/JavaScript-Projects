4638f45e3a3a70a9b544f0840eb1de5fceddeab1
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-20 17:13:02 -0500

    .js comments

--- Basic_JavaScript_Projects/Project7_scope_time_function/javascript/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/javascript/main.js
@@ -1,19 +1,19 @@
-var X = 10;
-function Add_numbers_1() {
-    document.getElementById("Number_one").innerHTML = (20 + X + "<br>");
+var X = 10;     //Declared global variable
+function Add_numbers_1() {  //Named function
+    document.getElementById("Number_one").innerHTML = (20 + X + "<br>");    //function using glabal variable
 }
-function Add_numbers_2() {
-    document.getElementById("Number_two").innerHTML = (X + 100);
+function Add_numbers_2() {  //Named function
+    document.getElementById("Number_two").innerHTML = (X + 100);    //Function using global variable
 }
 
 
-function Subtract_numbers_1() {
-    var Y = 50;
-    document.getElementById("Number_three").innerHTML = (Y - 10);
+function Subtract_numbers_1() {     //Named Function
+    var Y = 50;     //Declared local variable
+    document.getElementById("Number_three").innerHTML = (Y - 10);   //Function using local variable
 }
-function Subtract_numbers_2() {
-    document.getElementById("Number_four").innerHTML = (Y - 20);
+function Subtract_numbers_2() {     //Named Function
+    document.getElementById("Number_four").innerHTML = (Y - 20);    //Function attempting to use local variable
 }
-function Subtract_numbers_2() {
-    console.log(Y - 20);
+function Subtract_numbers_2() {     //Named function
+    console.log(Y - 20);    //Function to display error to console log
 }

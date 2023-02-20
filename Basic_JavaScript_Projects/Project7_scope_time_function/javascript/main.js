c1abe0ab5386a3c3c03d125e946051a29972365f
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-20 17:02:24 -0500

    glabal variable function

---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/javascript/main.js
@@ -0,0 +1,10 @@
+var X = 10;
+function Add_numbers_1() {
+    document.getElementById("Number_one").innerHTML = (20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.getElementById("Number_two").innerHTML = (X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+

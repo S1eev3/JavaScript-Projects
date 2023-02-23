70d7eb7db1d86478a5f447cd1014c048018261c7
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-23 16:11:51 -0500

    Time_Function

--- Basic_JavaScript_Projects/Project7_scope_time_function/javascript/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/javascript/main.js
@@ -41,3 +41,18 @@ function Age_Function() {
     }
     document.getElementById("How_old_are_you?").innerHTML = Rental;
 }
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}

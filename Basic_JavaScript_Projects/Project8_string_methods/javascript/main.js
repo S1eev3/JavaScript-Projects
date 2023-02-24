8f6b13a6a6fcef4a30d14a87d0d92c6eb175007e
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-24 12:25:49 -0500

    .js comments

--- Basic_JavaScript_Projects/Project8_string_methods/javascript/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/javascript/main.js
@@ -1,46 +1,46 @@
-function full_Sentence() {
+function full_Sentence() {      //Named function
     var part_1 = "I have ";
     var part_2 = "made this ";
     var part_3 = "into a complete ";
-    var part_4 = "sentence.";
+    var part_4 = "sentence.";       //Declared 4 variables to concatenate
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concatenate").innerHTML = whole_sentence;
+    document.getElementById("Concatenate").innerHTML = whole_sentence;  //Display concatenated sentence
 }
 
-function slice_Method() {
-    var Sentence = "All work and no play makes Johnny a dull boy.";
-    var Section = Sentence.slice(27, 33);
-    document.getElementById("Slice").innerHTML = Section;
+function slice_Method() {       //Named function
+    var Sentence = "All work and no play makes Johnny a dull boy."; //Declared Sentence variable
+    var Section = Sentence.slice(27, 33);   //Declared section variable using slice method
+    document.getElementById("Slice").innerHTML = Section;   //Display result of slice method
 }
 
-function case_Upper() {
-    var text = "Here is some text.";
-    var result = text.toUpperCase();
-    document.getElementById("Upper").innerHTML = result;
+function case_Upper() {     //Named function
+    var text = "Here is some text.";    //Declared text variable
+    var result = text.toUpperCase();    //Declared result variable using value from toUpperCase method
+    document.getElementById("Upper").innerHTML = result;    //Display value of result
 }
 
-function word_Position() {
-    var text = "I have a cat and a dog.";
-    var result = text.search("dog");
-    document.getElementById("search").innerHTML = result;
+function word_Position() {      //Named function
+    var text = "I have a cat and a dog.";   //Declared text variable
+    var result = text.search("dog");        //Declared result variable using value from search method
+    document.getElementById("search").innerHTML = result;   //Display value of result
 }
 
-function string_Method() {
-    var X = 182;
-    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+function string_Method() {      //Named function
+    var X = 182;        //Declared variable X and gave value
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();  //Display value of toString method
 }
 
-function precision_Method() {
-    var X = 12938.3012987376112;
-    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+function precision_Method() {       //Named function
+    var X = 12938.3012987376112;    //Declared variable X and gave value
+    document.getElementById("Precision").innerHTML = X.toPrecision(10); //Use toPrecision method on X and display result
 }
 
-function fixedMethod() {
-    var Y = 78.5926543;
-    document.getElementById("Fixed").innerHTML = Y.toFixed(3);
+function fixedMethod() {        //Named function
+    var Y = 78.5926543;     //Declared variable Y and gave value
+    document.getElementById("Fixed").innerHTML = Y.toFixed(3);  //Use toFixed method on Y and display result
 }
 
-function valueString() {
-    var Z = "JavaScript Project 8";
-    document.getElementById("String_Value").innerHTML = Z.valueOf();
+function valueString() {        //Named function
+    var Z = "JavaScript Project 8";     //Declared variable Z and gave value
+    document.getElementById("String_Value").innerHTML = Z.valueOf();    //Use valueOf method on Z and display result
 }

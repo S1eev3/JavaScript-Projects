8735540f5f375cd5bdf96478bcc9f0289a082cc7
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-02-24 13:33:37 -0500

    .js comments

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/javascript/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/javascript/main.js
@@ -1,21 +1,21 @@
-function countdown() {
-    var seconds = document.getElementById("seconds").value;
+function countdown() {      //Named function
+    var seconds = document.getElementById("seconds").value; //Declared variable seconds with value from user input
 
-    function tick() {
-        seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
-            alert("Times up!");
-            clearTimeout(time);
-            timer.innerHTML = "";
+    function tick() {   //Named function
+        seconds = seconds - 1;  //Alter seconds value
+        timer.innerHTML = seconds;  //Display changing value of seconds
+        var time = setTimeout(tick, 1000);  //Declared variable time with value using setTimeout and tick functions
+        if (seconds == -1) {    //If statement to determine timer finished
+            alert("Times up!"); //Alert when if becomes true
+            clearTimeout(time); //clearTimeout method
+            timer.innerHTML = "";   //clear timer display
         }
     }
-    tick();
+    tick();     //tick function to countdown
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1;     //Declared variable slideIndex and gave initial value
+showSlides(slideIndex); //Named function showSlides with slideIndex parameters
 
 //Next/previous controls
 function plusSlides(n) {
@@ -27,11 +27,12 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+//Named and created all variables and statements for showSlides function
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
+    let i;      
+    let slides = document.getElementsByClassName("mySlides");   
+    let dots = document.getElementsByClassName("dot");      
+    if (n > slides.length) { slideIndex = 1 }   
     if (n < 1) { slideIndex = slides.length }
     for (i = 0, i < slides.length; i++;) {
         slides[i].style.display = "none";
